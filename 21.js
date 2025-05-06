642c941dc04ebb5ef806d676af9b315f61b637a2
AzamatUzakov <azamatakmal053@gmail.com>
2025-05-06 18:29:44 +0500

    Initial commit

---
+++ 21.js
@@ -0,0 +1,29 @@
+//Задание 1. Создание диапазона
+//function range(start, end) {
+//  let arr = [];
+//
+//  for (let i = start; i < end + 1; i++) {
+//    arr.push(i);
+//  }
+//  console.log(arr);
+//}
+//range(0, 5);
+//-------------------------------------------------------------------------//
+//Задание 2. Поиск двух чисел, составляющих сумму
+//function getNumbersIdBySum(arrayOfNumbers, sum) {
+//  for (let i = 0; i < arrayOfNumbers.length; i++) {
+//    for (let g = arrayOfNumbers[i]; g < arrayOfNumbers.length; g++) {
+//      if (arrayOfNumbers[i] + arrayOfNumbers[g] === sum) {
+//        console.log(arrayOfNumbers[i], arrayOfNumbers[g]);
+//      } else {
+//        console.log(null);
+//      }
+//    }
+//  }
+//}
+//getNumbersIdBySum([1, 0], 5);
+
+
+
+
+
