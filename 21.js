24331c17bdc6429e907e4103025b00c61c62bd6e
AzamatUzakov <azamatakmal053@gmail.com>
2025-05-08 18:25:59 +0500

    improve task 2

--- 21.js
+++ 21.js
@@ -10,18 +10,18 @@
 //range(0, 5);
 //-------------------------------------------------------------------------//
 //Задание 2. Поиск двух чисел, составляющих сумму
-//function getNumbersIdBySum(arrayOfNumbers, sum) {
-//  for (let i = 0; i < arrayOfNumbers.length; i++) {
-//    for (let g = arrayOfNumbers[i]; g < arrayOfNumbers.length; g++) {
-//      if (arrayOfNumbers[i] + arrayOfNumbers[g] === sum) {
-//        console.log(arrayOfNumbers[i], arrayOfNumbers[g]);
-//      } else {
-//        console.log(null);
-//      }
-//    }
-//  }
-//}
-//getNumbersIdBySum([1, 0], 5);
+function getNumbersIdBySum(arrayOfNumbers, sum) {
+  for (let i = 0; i < arrayOfNumbers.length; i++) {
+    for (let g = arrayOfNumbers[i]; g < arrayOfNumbers.length; g++) {
+      if (arrayOfNumbers[i] + arrayOfNumbers[g] === sum) {
+        console.log(arrayOfNumbers[i], arrayOfNumbers[g]);
+      } else {
+        console.log(null);
+      }
+    }
+  }
+}
+getNumbersIdBySum([1, 0], 5);
 
 
 
