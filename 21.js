820c2650aa8fc794c1824a885bb2e945c6adce40
AzamatUzakov <azamatakmal053@gmail.com>
2025-06-13 14:50:42 +0500

    Задание 4. Нахождение простых чисел

--- 21.js
+++ 21.js
@@ -44,24 +44,29 @@
 //-------------------------------------------------------------------------//
 
 //Задание 4. Нахождение простых чисел
-/* 
-function getSimpleNumbers(touple) {
-  let start_num = touple[0];
-  let end_num = touple.at(-1);
-  let prostie_chisla = [];
-  for (let i = start_num; i <= end_num; i++) {
-    console.log(i);
 
-    for (let g = i + 1; g <= end_num; g++) {
-      if (g % i === 0) {
-        console.log(i);
-      } else {
-        prostie_chisla.push(g);
+function getSimpleNumbers(touple) {
+  let first_number = touple[0]; //беру первый элемент из массива
+  let second_number = touple.at(-1); //беру последний элемент из массива
+  let all_nums = [];
+  for (let i = first_number; i <= second_number; i++) { //делаю цикл чтобы вывести диапазон
+    all_nums.push(i);
+  }
+
+  for (let g = 0; g < all_nums.length; g++) {//первый цикл по массиву
+    if (all_nums[g] !== false) {//проверяю если числа простые
+      for (let f = g + 1; f < all_nums.length; f++) {//второй цикл по маасиву, начиная с g + 1 элемента
+        if (all_nums[f] % all_nums[g] === 0) {//беру эмент и нахожу все кратные ему числа
+          all_nums[f] = false; // если число не простое то даю ему fasle
+        }
       }
     }
   }
-  console.log(prostie_chisla);
+  return all_nums.filter((n) => {
+    if (n !== false) {
+      console.log(n);
+    }
+  });
 }
 
-getSimpleNumbers([2, 10]);
- */
+getSimpleNumbers([2, 10]); //2 3 5 7
