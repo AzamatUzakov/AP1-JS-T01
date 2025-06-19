89778bd7f6cdd894ee79edfda4a23a2f008a87c2
AzamatUzakov <azamatakmal053@gmail.com>
2025-06-19 16:19:19 +0500

    process

---
+++ meeting-event.js
@@ -0,0 +1,15 @@
+/* const dayStart = "07:30"; // начало дня
+const dayEnd = "17:45"; // конец дня;
+
+let [start_hours, start_min] = dayStart.split(":").map(Number); // разделяю строку через : и перевожу его в number;
+let [end_hours, end_min] = dayEnd.split(":").map(Number); // разделяю строку через : и перевожу его в number;
+
+function scheduleMeeting(startTime, durationMinutes) {
+    let [startTime_hours, startTime_min] = startTime.split(":").map(Number);
+    
+
+  console.log(startTime_hours, startTime_min);
+}
+
+scheduleMeeting("07:30", 15);
+ */
