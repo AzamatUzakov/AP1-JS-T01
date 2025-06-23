0cd8a37effb6ed230a65baafe108528d93fbf9c6
AzamatUzakov <azamatakmal053@gmail.com>
2025-06-23 18:03:22 +0500

    1

--- meeting-event.js
+++ meeting-event.js
@@ -1,6 +1,6 @@
 const dayStart = "07:30"; // начало дня
 const dayEnd = "17:45"; // конец дня;
-/* 
+
 let [start_hours, start_min] = dayStart.split(":").map(Number); // разделяю строку через : и перевожу его в number;
 let [end_hours, end_min] = dayEnd.split(":").map(Number); // разделяю строку через : и перевожу его в number;
 
@@ -12,4 +12,3 @@ function scheduleMeeting(startTime, durationMinutes) {
 }
 
 scheduleMeeting("07:30", 15);
- */
