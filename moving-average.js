34f1fb745653a04e0e2d67d461674e371c9b1957
AzamatUzakov <azamatakmal053@gmail.com>
2025-07-10 18:18:18 +0500

    create

---
+++ moving-average.js
@@ -0,0 +1,10 @@
+//Задание 7. Скользящее среднее
+function sma(arrayOfNumbers, period) {
+    let newArr = []
+
+    for (let i = 0; i < arrayOfNumbers.length; i++) {
+
+    }
+
+}
+sma([1, 2, 3], 3)
