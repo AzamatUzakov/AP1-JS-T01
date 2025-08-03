94672e7b9952b76a57b7590210ba79768cc4a307
AzamatUzakov <azamatakmal053@gmail.com>
2025-08-03 22:32:11 +0500

    Задание 7. Скользящее среднее

--- moving-average.js
+++ moving-average.js
@@ -1,10 +1,15 @@
 //Задание 7. Скользящее среднее
 function sma(arrayOfNumbers, period) {
-    let newArr = []
-
+    let smaArr = []
     for (let i = 0; i < arrayOfNumbers.length; i++) {
-
+        let start = i - period + 1//Используется для скользящего среднего, чтобы взять period элементов, заканчивающихся на позиции i.
+        let sliceNum = arrayOfNumbers.slice(start, period + 1)
+        let sum = sliceNum.reduce((a, b) => a + b, 0) / period
+        smaArr.push(sum)
+        console.log(smaArr);
     }
-
-}/*  */
-sma([1, 2, 3], 3)
+}
+sma([1, 2, 3], 2)
+//Время выполнения: O(n × p) n — длина массива, p — длина периода (slice и reduce внутри цикла)
+//Сложность: 5/10
+//Описание: Вычисляет скользящее среднее: на каждом шаге берёт period последних чисел, считает их среднее и сохраняет в результат.
